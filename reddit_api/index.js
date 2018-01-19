8756d2c365078c3f50b9c83a6645a9b5c7d6adbd
KithStrelets <kith.strelets@gmail.com>
2018-01-19 18:26:18 +0200

    added a single-page posts set

--- reddit_api/index.js
+++ reddit_api/index.js
@@ -4,3 +4,40 @@ module.exports = {
 	version: '1.0.0',
 	author: 'Nikita'
 }
+
+const https = require("https");
+const posts = new Set();
+/////////////////request/////////////////
+const url =
+  "https://www.reddit.com/r/dankmemes/search.json?restrict_sr=on&t=all";
+https.get(url, res => {
+  res.setEncoding("utf8");
+  let body = "";
+  res.on("data", data => {
+    body += data;
+  });
+  res.on("end", () => {
+    body = JSON.parse(body);
+    let children = body.data.children;
+      children.forEach(function(item) {
+          let objPost = new Object();
+          objPost.url = item.data.url;
+          objPost.title = item.data.title;
+          objPost.subreddit = item.data.subreddit;
+          posts.add(objPost);
+      });
+    
+    console.log(
+      posts
+    );
+  });
+});
+/////////////////server/////////////////
+var http = require("http");
+http.createServer(function(request,response){
+     
+    response.end('Memes Squad ');
+     
+}).listen(8080, "ec2-52-15-225-70.us-east-2.compute.amazonaws.com", function(){
+    console.log("started listening to 8080 ec2-52-15-225-70.us-east-2.compute.amazonaws.com");
+});
