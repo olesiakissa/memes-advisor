9bddb5f02d82dce6eb33b878c20337297a5d2eeb
Darya Nekryach <daryanekryach@gmail.com>
2018-01-21 20:44:55 +0200

    added operations with json

--- crud/index.js
+++ crud/index.js
@@ -1,6 +1,37 @@
 // info
 module.exports = {
-	name: 'crud',
-	version: '1.0.0',
+    name: 'crud',
+    version: '1.0.0',
     author: 'Dasha'
+}
+
+const fs = require("fs");
+
+// Receives Set and parses it to json
+module.exports.handleDataSet = function (dataSet) {
+    var arrayFromSet = Array.from(dataSet);
+    var json = JSON.stringify(arrayFromSet, null, 2);
+    saveToFile(json);
+};
+
+// Returns Set from JSON that is stored in file
+module.exports.handleJson = function () {
+    var json = getFromFile();
+    var parsedArray = JSON.parse(json);
+    let dataSet = new Set(parsedArray);
+    return dataSet
+};
+
+//Writes JSON to file
+function saveToFile(jsonData) {
+    fs.writeFile('crud/data/example.json', jsonData, function (err) {
+        if (err) {
+            return console.error(err);
+        }-
+    });
+}
+
+//Gets JSON from file
+function getFromFile() {
+    return fs.readFileSync('crud/data/example.json', 'utf8');
 }
